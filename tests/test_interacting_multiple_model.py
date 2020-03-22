a09666c9dda427f2f541ad2f33c77bfb7cc7b912
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-03-22 19:49:15 +0100

    Added test cases which compares the prediction and update of the IMM to the implementation in the filterpy lib

--- tests/test_interacting_multiple_model.py
+++ tests/test_interacting_multiple_model.py
@@ -6,6 +6,9 @@ from src.InteractingMultipleModel import InteractingMultipleModel
 from src.Filters import ExtendedKalmanFilter
 from src.Filters import KalmanFilter
 
+from filterpy.kalman import IMMEstimator as InteractingMultipleModel_filterpy
+from filterpy.kalman import KalmanFilter as KalmanFilter_filterpy
+
 class TestInteractingMultipleModel(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
@@ -32,10 +35,13 @@ class TestInteractingMultipleModel(unittest.TestCase):
         measurement_uncertainty_ekf = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])  # R
 
         self.initial_mode_probability = np.array([0.5, 0.5])
-        self.markov_transition_matrix = np.array([[1, 1,], [1, 0]])
+        self.markov_transition_matrix = np.array([[0.97, 0.03], [0.03, 0.97]])
 
         kf = KalmanFilter(state_kf, input_kf, measurement_kf, transition_kf, input_control_matrix_kf, noise_kf,
                           covariance_matrix_kf, measurement_matrix_kf, measurement_uncertainty_kf)
+
+        kf1 = KalmanFilter(state_ekf, input_ekf, measurement_ekf, transition_ekf, input_control_matrix_ekf, noise_ekf,
+                                  covariance_matrix_ekf, measurement_matrix_ekf, measurement_uncertainty_ekf)
 
         ekf = ExtendedKalmanFilter(state_ekf, input_ekf, measurement_ekf, transition_ekf,
                                    input_control_matrix_ekf, noise_ekf, covariance_matrix_ekf,
@@ -44,6 +50,28 @@ class TestInteractingMultipleModel(unittest.TestCase):
         self.filter = [kf, ekf]
 
         self.measuremnet = np.array([1,2,0,1])
+
+        # Used for comparing with IMM from filterpy
+        kf_filterpy1 = KalmanFilter_filterpy(4, 4, 4)
+        kf_filterpy1.x = state_kf
+        kf_filterpy1.P = covariance_matrix_kf
+        kf_filterpy1.F = transition_kf
+        kf_filterpy1.R = measurement_uncertainty_kf
+        kf_filterpy1.B = input_control_matrix_kf
+        kf_filterpy1.H = measurement_matrix_kf
+        kf_filterpy1.Q = noise_kf
+
+        kf_filterpy2 = KalmanFilter_filterpy(4, 4, 4)
+        kf_filterpy2.x = state_ekf
+        kf_filterpy2.P = covariance_matrix_ekf
+        kf_filterpy2.F = transition_ekf
+        kf_filterpy2.R = measurement_uncertainty_ekf
+        kf_filterpy2.B = input_control_matrix_ekf
+        kf_filterpy2.H = measurement_matrix_ekf
+        kf_filterpy2.Q = noise_ekf
+
+        self.filterpy_filters = [kf_filterpy1, kf_filterpy2]
+        self.kf_filters       = [kf, kf1]
 
     ##############################################################################
 
@@ -131,9 +159,34 @@ class TestInteractingMultipleModel(unittest.TestCase):
 
     ##############################################################################
 
-    def test_simple_system(self):
-        "TODO: implement a simple system and calculate 1 timestep. Verify all matrices afterwards"
-        pass
+    def test_compare_to_filterpy(self):
+        # We can only test the KF since the filterpy IMM only works with KF filters
+        imm = InteractingMultipleModel(self.kf_filters, self.initial_mode_probability, self.markov_transition_matrix)
+        imm_filterpy = InteractingMultipleModel_filterpy(self.filterpy_filters, self.initial_mode_probability, self.markov_transition_matrix)
+
+        # Predict update using implemented IMM
+        imm.predict_update(self.measuremnet, input=np.array([2, 2, 0, 0]))
+
+        # Predict update using filterpy update
+        imm_filterpy.predict(u=np.array([2, 2, 0, 0]))
+        imm_filterpy.update(z=self.measuremnet)
+
+        state_imm               = imm.state.copy()
+        state_imm_filterpy      = imm_filterpy.x.copy()
+        covariance_imm          = imm.covariance.copy()
+        covariance_imm_filterpy = imm_filterpy.P.copy()
+
+        # The states and covariances must be within a certain range of each other.
+        # Since the likelihood can be calculated differently in the filterpy IMM,
+        # it is highly unlikely that both values are equal.
+        # As long as we are within a reasonable range we are fine
+        state_difference             = np.abs(np.subtract(state_imm, state_imm_filterpy))
+        covariance_difference         = np.abs(np.subtract(covariance_imm, covariance_imm_filterpy))
+        maximum_state_difference      = np.amax(state_difference)
+        maximum_covariance_difference = np.amax(covariance_difference)
+
+        self.assertTrue(maximum_state_difference < 0.1)
+        self.assertTrue(maximum_covariance_difference < 0.15)
 
 if __name__ == '__main__':
     unittest.main()
