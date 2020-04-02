731b495231512b480489b0e46455d4c2d2efb4cc
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-04-02 23:36:44 +0200

    Created a basic test to check if the configuration can be loaded correctly

---
+++ tests/test_configuration_parser.py
@@ -0,0 +1,20 @@
+import unittest
+from src.ConfigParser import ConfigurationParser as CP
+
+CFGPATH="D:\\programming\\pycharm\\Masterarbeit\\MA\\config\\imm_cfg.cfg"
+
+class TestConfigurationParser(unittest.TestCase):
+    def test_config_read(self):
+        config = CP(cfg_path=CFGPATH)
+        print(config.filters)
+        print(config.state_variables)
+        print(config.markov_transition_matrix)
+        print(config.mode_probabilities)
+        print(config.filter_configs["KF"].transition_matrix)
+        print(config.filter_configs["KF1"].transition_matrix)
+        print(config.filter_configs["EKF"].transition_matrix)
+        print(config.filter_configs["EKF1"].transition_matrix)
+
+
+if __name__ == '__main__':
+    unittest.main()
