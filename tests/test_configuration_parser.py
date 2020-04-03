1be5a372409fe2bf099a3df2d5348d234db5c740
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-04-03 20:50:44 +0200

    use 3model config from: Comparison between Kalman Filter and Interacting Multiple MOdel using 2-D trajectories

--- tests/test_configuration_parser.py
+++ tests/test_configuration_parser.py
@@ -1,7 +1,7 @@
 import unittest
 from src.ConfigParser import ConfigurationParser as CP
 
-CFGPATH="D:\\programming\\pycharm\\Masterarbeit\\MA\\config\\imm_test_cfg.cfg"
+CFGPATH="D:\\programming\\pycharm\\Masterarbeit\\MA\\config\\imm_3models.cfg"
 
 class TestConfigurationParser(unittest.TestCase):
     def test_config_read(self):
@@ -11,10 +11,17 @@ class TestConfigurationParser(unittest.TestCase):
         print(config.markov_transition_matrix)
         print(config.mode_probabilities)
         print(config.filter_configs["KF"].transition_matrix)
-        print(config.filter_configs["KF1"].transition_matrix)
-        print(config.filter_configs["EKF"].transition_matrix)
+        print(config.filter_configs["KF"].measurement_control_matrix)
+        print(config.filter_configs["KF"].input_control_matrix)
+        print(config.filter_configs["KF"].process_noise_matrix)
         print(config.filter_configs["EKF1"].transition_matrix)
-
+        print(config.filter_configs["EKF1"].measurement_control_matrix)
+        print(config.filter_configs["EKF1"].input_control_matrix)
+        print(config.filter_configs["EKF1"].process_noise_matrix)
+        print(config.filter_configs["EKF2"].transition_matrix)
+        print(config.filter_configs["EKF2"].measurement_control_matrix)
+        print(config.filter_configs["EKF2"].input_control_matrix)
+        print(config.filter_configs["EKF2"].process_noise_matrix)
 
 if __name__ == '__main__':
     unittest.main()
