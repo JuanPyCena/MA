8aa8944088c75ef0aec2d8e1df983552b42c7ad0
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-04-28 13:41:52 +0200

    created a class to use IMM for the testbench

---
+++ src/testbench/TestbenchIMM.py
@@ -0,0 +1,13 @@
+from src.utils.Decorators import *
+
+class TestbenchIMM(object):
+
+    @typecheck(dict())
+    def __init__(self, test_data):
+        pass
+
+    ##############################################################################
+
+    def run(self):
+        pass
+    # Todo: use this function to run the IMM using the provided testdata
