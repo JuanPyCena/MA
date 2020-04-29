8f7cc7ed0ef967de39e8a107f0216709166e5ec9
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-04-29 15:35:34 +0200

    simulate and plot multiple test sets

--- src/testbench/testbench.py
+++ src/testbench/testbench.py
@@ -13,7 +13,31 @@ def main():
 
     plotter.plot_test_data()
     plotter.plot_imm_data()
-    # plotter.plot_combined()
+    plotter.plot_combined()
+
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_constant_turning_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_constant_turning_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
+
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_constant_linear_acceleration_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_constant_linear_acceleration_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
 
 if __name__ == "__main__":
     main()
