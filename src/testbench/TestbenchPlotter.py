6d4b687e0324c2d1ed8e2eb17483f003b2fe286a
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-04-29 15:10:47 +0200

    fixed plotter

--- src/testbench/TestbenchPlotter.py
+++ src/testbench/TestbenchPlotter.py
@@ -11,9 +11,9 @@ class TestbenchPlotter(object):
         # Determine path were figures should be saved to
         _, self.plot_name_test_data = test_data_file.rsplit("\\", 1)
         _, self.plot_name_imm_data = imm_data_file.rsplit("\\", 1)
+        self.plot_name_combined_data = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\plots\\" + self.plot_name_imm_data[:-4]  + "_combined_plot"
         self.plot_name_test_data = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\plots\\" + self.plot_name_test_data[:-4] + "_test_data_plot"
         self.plot_name_imm_data = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\plots\\" + self.plot_name_imm_data[:-4]  + "_imm_data_plot"
-        self.plot_name_combined_data = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\plots\\" + self.plot_name_imm_data[:-4]  + "_combined_plot"
 
         # Read test data
         self.test_data = DFI.read_test_data(test_data_file)
@@ -53,8 +53,8 @@ class TestbenchPlotter(object):
         for data_point in state_data:
             pos, vel, acc = [], [], []
             pos.append(data_point[0])
-            pos.append(data_point[1])
-            vel.append(data_point[2])
+            vel.append(data_point[1])
+            pos.append(data_point[2])
             vel.append(data_point[3])
             acc.append(data_point[4])
             acc.append(data_point[5])
@@ -73,8 +73,8 @@ class TestbenchPlotter(object):
         for data_point in state_errors:
             pos, vel, acc = [], [], []
             pos.append(data_point[0])
-            pos.append(data_point[1])
-            vel.append(data_point[2])
+            vel.append(data_point[1])
+            pos.append(data_point[2])
             vel.append(data_point[3])
             acc.append(data_point[4])
             acc.append(data_point[5])
@@ -142,40 +142,60 @@ class TestbenchPlotter(object):
         mode_prob_2       = [prob[1] for prob in self.imm_data["mode_probabilities"]]
         mode_prob_3       = [prob[2] for prob in self.imm_data["mode_probabilities"]]
 
-        fig, axs = plt.subplots(2, 4)
+        fig, axs = plt.subplots(3)
+        axs[0].plot(x_pos_measurement, y_pos_measurement)
+        axs[0].set_title('measurement_position')
 
-        axs[0, 0].plot(x_pos_measurement, y_pos_measurement)
-        axs[0, 0].set_title('measurement_position')
+        axs[1].plot(x_vel_measurement, y_vel_measurement)
+        axs[1].set_title('measurement_velocity')
 
-        axs[0, 1].plot(x_vel_measurement, y_vel_measurement)
-        axs[0, 1].set_title('measurement_velocity')
-
-        axs[0, 2].plot(x_acc_measurement, y_acc_measurement)
-        axs[0, 2].set_title('measurement_acceleration')
-
-        axs[0, 3].plot(self.test_data["time"], mode_prob_1, label="Mode1")
-        axs[0, 3].plot(self.test_data["time"], mode_prob_2, label="Mode2")
-        axs[0, 3].plot(self.test_data["time"], mode_prob_3, label="Mode3")
-        axs[0, 3].set_title('mode_probabilities')
-        axs[0, 3].legend()
-
-        axs[1, 0].plot(x_pos_state, y_pos_state)
-        axs[1, 0].set_title('state_position')
-
-        axs[1, 1].plot(x_vel_state, y_vel_state)
-        axs[1, 1].set_title('state_velocity')
-
-        axs[1, 2].plot(x_acc_state, y_acc_state)
-        axs[1, 2].set_title('state_acceleration')
-
-        axs[1, 3].plot(x_pos_error, y_pos_error, label="Error Position")
-        axs[1, 3].plot(x_vel_error, y_vel_error, label="Error Velocity")
-        axs[1, 3].plot(x_acc_error, y_acc_error, label="Error Acceleration")
-        axs[1, 3].set_title('state_errors')
-        axs[1, 3].legend()
+        axs[2].plot(x_acc_measurement, y_acc_measurement)
+        axs[2].set_title('measurement_acceleration')
 
         fig.tight_layout()
-        plt.savefig(self.plot_name_imm_data, dpi=1200)
+        plt.savefig(self.plot_name_imm_data + "_measurements", dpi=1200)
+        plt.show()
+
+        fig, axs = plt.subplots(3)
+        axs[0].plot(x_pos_state, y_pos_state)
+        axs[0].set_title('state_position')
+
+        axs[1].plot(x_vel_state, y_vel_state)
+        axs[1].set_title('state_velocity')
+
+        axs[2].plot(x_acc_state, y_acc_state)
+        axs[2].set_title('state_acceleration')
+        fig.tight_layout()
+        plt.savefig(self.plot_name_imm_data + "_state", dpi=1200)
+        plt.show()
+
+        fig, axs = plt.subplots(1)
+        axs.plot(self.test_data["time"][:-1], mode_prob_1, label="Mode1")
+        axs.plot(self.test_data["time"][:-1], mode_prob_2, label="Mode2")
+        axs.plot(self.test_data["time"][:-1], mode_prob_3, label="Mode3")
+        axs.set_title('mode_probabilities')
+        axs.legend()
+
+        fig.tight_layout()
+        plt.savefig(self.plot_name_imm_data + "_mode", dpi=1200)
+        plt.show()
+
+        fig, axs = plt.subplots(3)
+        axs[0].plot(self.test_data["time"][:-1], x_pos_error, label="Position-x")
+        axs[0].plot(self.test_data["time"][:-1], y_pos_error, label="Position-y")
+        axs[0].set_title('Position errors')
+        axs[0].legend()
+        axs[1].plot(self.test_data["time"][:-1], x_vel_error, label="Velocity-x")
+        axs[1].plot(self.test_data["time"][:-1], y_vel_error, label="Velocity-y")
+        axs[1].set_title('Velocity errors')
+        axs[1].legend()
+        axs[2].plot(self.test_data["time"][:-1], x_acc_error, label="Acceleration-x")
+        axs[2].plot(self.test_data["time"][:-1], y_acc_error, label="Acceleration-y")
+        axs[2].set_title('Acceleration errors')
+        axs[2].legend()
+
+        fig.tight_layout()
+        plt.savefig(self.plot_name_imm_data + "_errors", dpi=1200)
         plt.show()
 
         print("Finished Plotting IMM data")
@@ -196,32 +216,23 @@ class TestbenchPlotter(object):
         y_vel_imm_state = [vel[1] for vel in self.imm_data["velocity_state"]]
         x_acc_imm_state = [acc[0] for acc in self.imm_data["acceleration_state"]]
         y_acc_imm_state = [acc[1] for acc in self.imm_data["acceleration_state"]]
-        x_pos_error = [pos[0] for pos in self.imm_data["position_error"]]
-        y_pos_error = [pos[1] for pos in self.imm_data["position_error"]]
-        x_vel_error = [vel[0] for vel in self.imm_data["velocity_error"]]
-        y_vel_error = [vel[1] for vel in self.imm_data["velocity_error"]]
-        x_acc_error = [acc[0] for acc in self.imm_data["acceleration_error"]]
-        y_acc_error = [acc[1] for acc in self.imm_data["acceleration_error"]]
 
-        fig, axs = plt.subplots(2, 2)
+        fig, axs = plt.subplots(3, 1)
 
-        axs[0, 0].plot(x_pos_test_data, y_pos_test_data, label="Position test data")
-        axs[0, 0].plot(x_pos_imm_state, y_pos_imm_state, label="Position imm state")
-        axs[0, 0].set_title('Position')
+        axs[0].plot(x_pos_test_data, y_pos_test_data, label="Position test data")
+        axs[0].plot(x_pos_imm_state, y_pos_imm_state, label="Position imm state")
+        axs[0].set_title('Position')
+        axs[0].legend()
 
-        axs[0, 1].plot(x_vel_test_data, y_vel_test_data, label="Velocity test data")
-        axs[0, 1].plot(x_vel_imm_state, y_vel_imm_state, label="Velocity imm state")
-        axs[0, 1].set_title('Velocity')
+        axs[1].plot(x_vel_test_data, y_vel_test_data, label="Velocity test data")
+        axs[1].plot(x_vel_imm_state, y_vel_imm_state, label="Velocity imm state")
+        axs[1].set_title('Velocity')
+        axs[1].legend()
 
-        axs[1, 0].plot(x_acc_test_data, y_acc_test_data, label="Acceleration test data")
-        axs[1, 0].plot(x_acc_imm_state, y_acc_imm_state, label="Acceleration imm state")
-        axs[1, 0].set_title('Acceleration')
-
-        axs[1, 1].plot(x_pos_error, y_pos_error, label="Error Position")
-        axs[1, 1].plot(x_vel_error, y_vel_error, label="Error Velocity")
-        axs[1, 1].plot(x_acc_error, y_acc_error, label="Error Acceleration")
-        axs[1, 1].set_title('Errors')
-        axs[1, 1].legend()
+        axs[2].plot(x_acc_test_data, y_acc_test_data, label="Acceleration test data")
+        axs[2].plot(x_acc_imm_state, y_acc_imm_state, label="Acceleration imm state")
+        axs[2].set_title('Acceleration')
+        axs[2].legend()
 
         fig.tight_layout()
         plt.savefig(self.plot_name_combined_data, dpi=1200)
