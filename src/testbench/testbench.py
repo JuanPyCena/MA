0e8a0c146636d587918dc6d61e97b1ebb7019566
Felix Sonnleitner <felix.sonnleitner@gmx.at>
2020-05-13 07:36:14 +0200

    simulate noisy and clean data

--- src/testbench/testbench.py
+++ src/testbench/testbench.py
@@ -3,6 +3,42 @@ from src.testbench.TestbenchIMM import TestbenchIMM
 from src.utils.DataFileInterface import DataFilteInterface as DataReadWrite
 
 def main():
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_linear_uniform_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_linear_uniform_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
+
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_constant_turning_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_constant_turning_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
+
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_constant_linear_acceleration_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_constant_linear_acceleration_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
+
     test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_linear_uniform_motion_noise.csv"
     imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_linear_uniform_motion_noise.csv"
     test_data = DataReadWrite.read_test_data(test_data_file)
@@ -38,18 +74,18 @@ def main():
     plotter.plot_test_data()
     plotter.plot_imm_data()
     plotter.plot_combined()
-    #
-    # test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_combined_motion.csv"
-    # imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_combined_motion.csv"
-    # test_data = DataReadWrite.read_test_data(test_data_file)
-    #
-    # imm = TestbenchIMM(test_data, imm_data_file)
-    # imm.run()
-    # plotter = TestbenchPlotter(test_data_file, imm_data_file)
-    #
-    # plotter.plot_test_data()
-    # plotter.plot_imm_data()
-    # plotter.plot_combined()
+
+    test_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\\src\\testbench\\test_data\\test_data_combined_motion.csv"
+    imm_data_file = "D:\\programming\\pycharm\\Masterarbeit\\MA\src\\testbench\\imm_data\\imm_data_combined_motion.csv"
+    test_data = DataReadWrite.read_test_data(test_data_file)
+
+    imm = TestbenchIMM(test_data, imm_data_file)
+    imm.run()
+    plotter = TestbenchPlotter(test_data_file, imm_data_file)
+
+    plotter.plot_test_data()
+    plotter.plot_imm_data()
+    plotter.plot_combined()
 
 if __name__ == "__main__":
     main()
